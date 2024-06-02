bcb1e4691782cd15d5416e3aedea3a1886c38666
Mikhail Sharin <mik.sharin@gmail.com>
2024-06-02 18:22:09 -0300

    update dependencies

--- app/src/main/kotlin/co/uk/basedapps/vpn/ui/screens/dashboard/widgets/MapboxConfiguredMap.kt
+++ app/src/main/kotlin/co/uk/basedapps/vpn/ui/screens/dashboard/widgets/MapboxConfiguredMap.kt
@@ -2,14 +2,10 @@ package co.uk.basedapps.vpn.ui.screens.dashboard.widgets
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.mapbox.maps.MapInitOptions
 import com.mapbox.maps.MapboxExperimental
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.MapViewportState
-import com.mapbox.maps.plugin.attribution.generated.AttributionSettings
-import com.mapbox.maps.plugin.compass.generated.CompassSettings
 import com.mapbox.maps.plugin.gestures.generated.GesturesSettings
-import com.mapbox.maps.plugin.scalebar.generated.ScaleBarSettings
 
 @OptIn(MapboxExperimental::class)
 @Composable
@@ -20,19 +16,17 @@ fun MapboxConfiguredMap(
   MapboxMap(
     modifier = modifier,
     mapViewportState = mapViewportState,
-    mapInitOptionsFactory = { context ->
-      MapInitOptions(
-        context = context,
-      )
-    },
     gesturesSettings = GesturesSettings {
       pitchEnabled = false
       scrollEnabled = false
       pinchToZoomEnabled = false
       rotateEnabled = false
+      doubleTapToZoomInEnabled = false
+      doubleTouchToZoomOutEnabled = false
+      quickZoomEnabled = false
     },
-    attributionSettings = AttributionSettings { enabled = false },
-    compassSettings = CompassSettings { enabled = false },
-    scaleBarSettings = ScaleBarSettings { enabled = false },
+    compass = {},
+    scaleBar = {},
+    attribution = {},
   )
 }
